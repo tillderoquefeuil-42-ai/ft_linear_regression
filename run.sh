a7fde43c82317ca6636fcc567ad905b0b1f65b09
Till De Roquefeuil <tillderoquefeuil@MacBook-Pro-de-Till.local>
2020-05-14 16:10:59 +0200

    run.sh corrections

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 printf "Install dependencies (numpy, pandas, matplotlib)?[Y/N]"
 read d_install
 
+d_install=$(echo $d_install | tr 'a-z' 'A-Z')
 if [ -z "$d_install" ] || [ $d_install == "Y" ]; then
     printf "\nInstalling numpy ..."
     pip3 install numpy -q
@@ -16,6 +17,7 @@ fi
 printf "\n\nRun program?[Y/N]"
 read p_run
 
+p_run=$(echo $p_run | tr 'a-z' 'A-Z')
 if [ -z "$p_run" ] || [ $p_run == "Y" ]; then
     python3 main.py
 fi
